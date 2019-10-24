32136fa2f491a8059755c7a4cb94e728bb21a4e6
Amber <13611718+mxamber@users.noreply.github.com>
2019-10-25 00:58:57 +0200

    initial upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+using AO3scrape;
+
+namespace AO3scrape
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Regex work_regex = new Regex(@"([\d]+)\sWorks\sin");
+			
+			Regex error_regex = new Regex("div[^\\>]*class=\"[\\w\\s]*errors");
+			
+			
+			Console.Write("Enter tag: ");
+			String tag = Console.ReadLine();
+			
+		min:
+			Console.Write("Enter min words: ");
+			int min = 0;
+			if(Int32.TryParse(Console.ReadLine(), out min) == false) {
+				Console.WriteLine("ERROR: invalid number! Please try again.");
+				goto min;
+			}
+			
+		max:
+			Console.Write("Enter max words: ");
+			int max = 0;
+			if(Int32.TryParse(Console.ReadLine(), out max) == false) {
+				Console.WriteLine("ERROR: invalid number! Please try again.");
+				goto max;
+			}
+			
+			String url = UrlGenerator.worksUrl(min, max, tag);
+			
+			String raw = Scraper.scrape(url);
+			
+			if(raw == null) {
+				Console.ReadLine();
+				return;
+			}
+			
+			int works = 0;
+			if(Int32.TryParse(work_regex.Match(raw).Groups[1].ToString().Trim(), out works) == false) {
+				Console.WriteLine("ERROR! Could not parse response!");
+				Console.ReadLine();
+				return;
+			}
+			
+			Console.WriteLine("There are " + works + " works tagged '" + tag + "' between " + min + " and " + max + " words.");
+			
+			Console.ReadLine();
+		}
+	}
+}
---
+++ UrlGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AO3scrape
+{
+	public class UrlGenerator
+	{
+		const String str1 = "https://archiveofourown.org/works?work_search[words_from]=";
+		const String str2 = "&work_search[words_to]=";
+		const String str3 = "&tag_id=";
+		
+		String return_url;
+		
+		public UrlGenerator(int min_words, int max_words, String tag_name)
+		{
+			this.return_url = str1 + min_words + str2 + max_words + str3 + tag_name;
+		}
+		
+		public static String worksUrl(int min_words, int max_words, String tag_name)
+		{
+			return(str1 + min_words + str2 + max_words + str3 + tag_name);
+		}
+	}
+}
