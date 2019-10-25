917cace2b2cca71921597c53da114db607c92ec7
Amber <13611718+mxamber@users.noreply.github.com>
2019-10-25 15:55:02 +0200

    text stuff, moving functionality into own function
    added exporter.cs
    added function getWorkNumbers
    added failsafes and exit commands

---
+++ Exporter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AO3scrape
+{
+	public class Exporter
+	{
+		public Exporter()
+		{
+			
+		}
+		
+		public static String addColumn(String input, String label, String value) {
+			input += Environment.NewLine;
+			input += label;
+			input += "\t\t";
+			input += value;
+			
+			return input;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -7,12 +7,31 @@ namespace AO3scrape
 {
 	class Program
 	{
+		public static int getWorkNumbers(String tag, int min, int max) {
+			Regex work_regex = new Regex(@"([\d]+)\sWorks\sin");
+			Regex error_regex = new Regex("div[^\\>]*class=\"[\\w\\s]*errors");
+			
+			int works = 0;												// number of works: default 0 until a number can be found
+			
+			String url = UrlGenerator.worksUrl(min, max, tag);			// generate search results URL
+			String raw = Scraper.scrape(url);							// scrape search results page 
+			
+			if(String.IsNullOrEmpty(raw) != true) {
+				Int32.TryParse(work_regex.Match(raw).Groups[1].ToString().Trim(), out works);
+				// if the response is not empty, regex for number and attempt to parse as int
+				// if successful: parsed number will be stored in var works, replacing default 0
+				// if unsuccessful, default 0 will remain				
+			}
+			
+			// either way, return number of works
+			// successful scraping will return number
+			// unsuccessful scraping will return 0
+			return works;
+		}
+		
+		
 		public static void Main(string[] args)
 		{
-			
-			Regex work_regex = new Regex(@"([\d]+)\sWorks\sin");
-			
-			Regex error_regex = new Regex("div[^\\>]*class=\"[\\w\\s]*errors");
 			
 			
 			Console.Write("Enter tag: ");
@@ -21,7 +40,11 @@ namespace AO3scrape
 		min:
 			Console.Write("Enter min words: ");
 			int min = 0;
-			if(Int32.TryParse(Console.ReadLine(), out min) == false) {
+			String min_t = Console.ReadLine();			// temporary value
+			if(Int32.TryParse(min_t, out min) == false) {
+				if(min_t.Trim() == "exit") {					// if the NaN value entered is "exit", shut down
+					return;
+				}
 				Console.WriteLine("ERROR: invalid number! Please try again.");
 				goto min;
 			}
@@ -29,29 +52,34 @@ namespace AO3scrape
 		max:
 			Console.Write("Enter max words: ");
 			int max = 0;
-			if(Int32.TryParse(Console.ReadLine(), out max) == false) {
+			String max_t = Console.ReadLine();			// temporary value
+			if(Int32.TryParse(max_t, out max) == false) {
+				if(max_t.Trim() == "exit") {					// if the NaN value entered is "exit", shut down
+					return;
+				}
 				Console.WriteLine("ERROR: invalid number! Please try again.");
 				goto max;
 			}
-			
-			String url = UrlGenerator.worksUrl(min, max, tag);
-			
-			String raw = Scraper.scrape(url);
-			
-			if(raw == null) {
-				Console.ReadLine();
-				return;
+			if(max > 500000) {
+				max = 500000;
+			}
+			if(max < min) {			// min is actually max
+				int temp = max;		// max and temp are both actually min
+				max = min;			// max is now truly max
+				min = temp;			// min gets new value from temp, the former max
 			}
 			
-			int works = 0;
-			if(Int32.TryParse(work_regex.Match(raw).Groups[1].ToString().Trim(), out works) == false) {
-				Console.WriteLine("ERROR! Could not parse response!");
-				Console.ReadLine();
-				return;
-			}
 			
-			Console.WriteLine("There are " + works + " works tagged '" + tag + "' between " + min + " and " + max + " words.");
 			
+			
+			
+			
+			
+
+			
+			
+			Console.WriteLine("There are " + getWorkNumbers(tag, min, max) + " works tagged '" + tag + "' between " + min + " and " + max + " words.");
+			Console.WriteLine("Press enter to terminate...");
 			Console.ReadLine();
 		}
 	}
